21dec289b20a537554c82dee824b76312d26f010
Ezra-code <njerukithinji872@gmail.com>
2022-08-20 07:55:35 +0300

    commit tree c655306a447d9460607c2b033fb30c05c18265a3

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+
+    def initialize(knowledge=[])
+        @knowledge = knowledge
+    end
+
+    def learn(know)
+        @knowledge << know
+    end
+
+    # def knowledge
+    #     @knowledge
+    # end
 
 end
+
